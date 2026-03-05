f3873339d7f010b5e5efecbf1303a08d76ff1527
Nils Skäre <nils.skare@outlook.com>
2026-03-05 12:32:36 +0100

    nödvändiga kommentarer

--- Discord/Program.cs
+++ Discord/Program.cs
@@ -3,16 +3,18 @@ using Discord;
 var builder = WebApplication.CreateBuilder(new WebApplicationOptions
 {
     Args = args,
+    // mappen för statiska filer (index.html, osv...)
     WebRootPath = "public"
 });
 
 var app = builder.Build();
-app.UseFileServer();
+app.UseFileServer(); // använd statiska filer
 
 var messages = new List<MessageDto>(){
     new MessageDto("danne", "hej")
 };
 
+// Get för meddelanden
 app.MapGet("/api/messages", () => new { messages });
 
 // Post för meddelanden
