e2a9997f5c3e2527352a5c4cc8ac2823d548b51b
daniel-viklund <166545775+daniel-viklund@users.noreply.github.com>
2026-03-05 14:37:17 +0100

    123

--- Discord/Program.cs
+++ Discord/Program.cs
@@ -15,7 +15,8 @@ var messages = new List<MessageDto>(){
     new("lennart", "och hå", 1296867189),
     new("xX_Gandalf_Xx", "YOU SHALL NOT POST!", 192837198),
     new("birgitta69", "är nån vaken?", 19283795810),
-    new("danne", "@birgitta69 jo, jag är vaken", 19283795811)
+    new("danne", "@birgitta69 jo, jag är vaken", 19283795811),
+    new("max", "@danne snacka inte med min brud!", 19283795811),
 };
 
 // Get för meddelanden
