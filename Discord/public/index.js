67df7319d08e27806a127dc262fc9face05ebdb8
Nils Skäre <nils.skare@outlook.com>
2026-03-05 11:36:14 +0100

    javascript post

--- Discord/public/index.js
+++ Discord/public/index.js
@@ -14,4 +14,32 @@ async function testGet() {
     }
 }
 
+function createMessage(username, message) {
+    return {
+        user: username,
+        message: message
+    };
+}
+
+async function postMsg(msg) {
+    try {
+        const response = await fetch("http://localhost:3000/api/messages", {
+            method: "POST",
+            body: JSON.stringify(msg),
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+        if (!response.ok) {
+              throw new Error(`Response status: ${response.status}`);
+        }
+        const result = await response.json();
+        console.log(result);
+    }
+    catch(error) {
+        console.error(error.message);
+    }
+}
+
 testGet();
+postMsg(createMessage("bingus", "hata floppa >:("));
