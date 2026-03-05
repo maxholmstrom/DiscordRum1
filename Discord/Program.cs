43fac5d375f4036288c7d4737292d1dde4d296f8
maxholmstrom <maxholmstrom@hotmail.se>
2026-03-05 10:14:25 +0100

    hej

--- Discord/Program.cs
+++ Discord/Program.cs
@@ -1,6 +1,12 @@
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+{
+    Args = args,
+    // Look for static files in webroot
+    WebRootPath = "public"
+});
+
 var app = builder.Build();
+app.UseFileServer();
 
-app.MapGet("/", () => "Hello World!");
-
-app.Run();
+//app.MapGet("/index.html", () => "Hello World!");
+app.Run("http://localhost:3000");
